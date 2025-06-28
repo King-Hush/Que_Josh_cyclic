dfc7d6ba83add0252a184890125c5aad3f73ada6
King-Hush <165049474+King-Hush@users.noreply.github.com>
2025-06-29 00:31:35 +0800

    Add files via upload

---
+++ bfs.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 100;
+    int adj[MAX][MAX];
+    int visited[MAX] = {0};
+    int parent[MAX] = {-1};
+    int queue[MAX];
+    int path[MAX];
+    int V;
+
+    cout << "Enter number of vertices: ";
+    cin >> V;
+
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            cin >> adj[i][j];
+
+    // Variables for BFS
+    bool cycleFound = false;
+    int cycleStart = -1, cycleEnd = -1;
+    int front = 0, rear = 0;
+
+    for (int start = 0; start < V; start++) {
+        if (visited[start] == 0) {
+            // Reset queue and parent
+            front = 0;
+            rear = 0;
+            queue[rear++] = start;
+            visited[start] = 1;
+            parent[start] = -1;
+
+            while (front < rear) {
+                int u = queue[front++];
+
+                for (int v = 0; v < V; v++) {
+                    if (adj[u][v]) {
+                        if (!visited[v]) {
+                            visited[v] = 1;
+                            parent[v] = u;
+                            queue[rear++] = v;
+                        } else if (v != parent[u]) {
+                            // A cycle is found
+                            cycleFound = true;
+                            cycleStart = v;
+                            cycleEnd = u;
+                            break;
+                        }
+                    }
+                }
+                if (cycleFound) break;
+            }
+        }
+        if (cycleFound) break;
+    }
+
+    // Output
+    if (cycleFound) {
+        cout << "\nCycle found using BFS!\n";
+
+        // Reconstruct cycle path
+        int temp = cycleEnd;
+        int index = 0;
+        path[index++] = cycleEnd;
+
+        while (temp != cycleStart) {
+            temp = parent[temp];
+            path[index++] = temp;
+        }
+
+        path[index++] = cycleEnd; // Complete the cycle
+
+        cout << "Vertices in cycle: ";
+        for (int i = index - 1; i >= 0; i--)
+            cout << path[i] << " ";
+        cout << endl;
+    } else {
+        cout << "\nNo cycle found in the graph using BFS.\n";
+    }
+
+    return 0;
+}
+
